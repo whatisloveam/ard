5527d8616652df25c773f907d4f40eb9b74dc3cf
Vladislav Melnikov <40246526+whatisloveam@users.noreply.github.com>
2024-02-18 05:39:27 +0500

    Update BT_LED.ino

--- BT_LED.ino
+++ BT_LED.ino
@@ -2,6 +2,7 @@ SoftwareSerial BTSerial(10, 11); // RX, TX
 
 void setup() 
 {
+  pinMode(13, OUTPUT);
   BTSerial.begin(9600);
   Serial.begin(9600);
   Serial.println("ARDUINO READY");
@@ -11,7 +12,18 @@ void loop()
 {
   if (BTSerial.available())  //пришло сообщение с блютуза
   {
-    Serial.write(BTSerial.read());
+    char c = BTSerial.read();
+    if(c == '0') 
+    {
+      digitalWrite(13, 0);
+      Serial.println("LED OFF");
+    }
+    if(c == '1') 
+    {
+      digitalWrite(13, 1);
+      Serial.println("LED ON");
+    }
+    Serial.write(c);
   }
   if (Serial.available()) //пришло сообщение с компа
   {
